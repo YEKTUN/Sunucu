842acc054fa387d9778f06188e012c156b6093b8
Ali Tunçer <alituncer847@gmail.com>
2025-07-07 14:03:26 +0300

    sdas

--- src/app/api/download/route.ts
+++ src/app/api/download/route.ts
@@ -6,7 +6,7 @@ export const runtime = 'nodejs';
 
 export async function GET() {
   try {
-    const filePath = path.join(process.cwd(), 'public', 'kaya.zip');
+    const filePath = path.join(process.cwd(), 'public', 'kaya.exe');
 
     const fileBuffer = await fs.readFile(filePath);
 
@@ -16,8 +16,8 @@ export async function GET() {
     return new NextResponse(uint8, {
       status: 200,
       headers: {
-        'Content-Type': 'application/zip',
-        'Content-Disposition': 'attachment; filename="kaya.zip"',
+        'Content-Type': 'application/octet-stream',
+        'Content-Disposition': 'attachment; filename="kaya.exe"',
       },
     });
   } catch (error) {
